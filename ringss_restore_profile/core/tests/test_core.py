a1f20cc7d4908346ff629e8d65d3abfe4e70ddcf
Simon Torres <simon.torres@noirlab.edu>
2022-05-25 16:01:17 -0400

    Added test for read_json

--- ringss_restore_profile/core/tests/test_core.py
+++ ringss_restore_profile/core/tests/test_core.py
@@ -1,5 +1,12 @@
+from unittest import TestCase
+
 from ..core import read_json
 
 
-def test_read_json():
-    pass
+class TestReadJsonFile(TestCase):
+
+    def test_read_json__file_does_not_exist(self):
+        no_file = 'file_that_does_not_exists.json'
+        data = read_json(filename=no_file)
+        self.assertIsNone(data)
+
